cb18b3a086e5a47b8e29ab8ade50accbd7156fb2
fwilcox <fewilcox3@gmail.com>
2020-02-07 15:48:42 -0500

    Updated to uise buzzwords

--- main.js
+++ main.js
@@ -1,3 +1,56 @@
+var buzzwords = new Array ("100% Replace",
+	'High 5 Aggy',
+	'Submit to Catalog Dog',
+	'Help a Customer',
+	'Restock Staging Area',
+	'Ask a Team Member for Help',
+	'Check Skype for New Msgs',
+	'Setup A Cart',
+	'Change Label Roll in Printer',
+	'Assisting Another Shopper',
+	'Wearing Amazon Swag',
+	'Comunicate Safety Concern',
+	'Tidy Staging Area',
+	'Help Driver Find a Bag',
+	'Shop an Order > 60',
+	'Introduce Yourself to New Associate',
+	'Shop a 1 Item Order',
+	'Todays UPH > Last 30 days',
+	'Shop an Item for 1st Time',
+	'Take out Trash',
+	'Say Hello to 5 WF Team Members',
+	'Put a Cart Away',
+	'Use Green Bag for Chemical',
+	'Help Stage an Order',
+	'Use Green Bag for Meat',
+	'Get Help from Customer Service',
+	'Shop an Order of All Produce',
+	'100% Product Found Today',
+	'Uncatagorized Item in Order',
+	'Order with 2 < Water Gallons',
+	'Stage "Bulky" Item Low',
+	'Arrive Ontime for Shift',
+	'Hydrate During Shift',
+	'Take "15" Minute Break',
+	'Fill Out Order Handoff Sheet',
+	'Read All Acknowledgements',
+	'Shop a "Taco Tuesday" Order',
+	'Complete Closing Checklist',
+	'Birthday Cake in Order',
+	'Fill Out Deli Sheet',
+	'Stage "Lighter" Item on Top Shelf',
+	'Shop Order > 99 Items',
+	'Answer Customer in Chat',
+	'All Sushi Order',
+	'Clean Shopper Cabinet',
+	'No Personal Items in Cart',
+	'Use Green Bag for Seafood',
+	'Found Item after Asking',
+	'Use Break Room',
+	'Remove Jam from Printer'
+	);
+
+var usedWords = new Array(buzzwords.length);
 window.onload = initAll;
 var usedNums = new Array(76);
 
@@ -18,18 +71,14 @@ function newCard() {
 }
 
 function setSquare(thisSquare) {
-		var currSquare = "square" + thisSquare;
-		var colPlace = new Array(0,0,0,0,0,1,1,1,1,1,2,2,2,2,2,3,3,3,3,3,4,4,4,4,4);
-		var colBasis = colPlace[thisSquare] * 15;
-		var newNum;
-
 		do {
-			newNum = colBasis + getNewNum() + 1;
+			var randomWord = Math.floor(Math.random() * buzzwords.length);
 		}
-		while (usedNums[newNum]);
+		while (usedWords[randomWord]);
 
-		usedNums[newNum] = true;
-		document.getElementById(currSquare).innerHTML = newNum;
+		usedWords[randomWord] = true;
+		var currSquare = "square" + thisSquare;
+		document.getElementById(currSquare).innerHTML = buzzwords[randomWord];
 		document.getElementById(currSquare).className = "";
 		document.getElementById(currSquare).onmousedown = toggleColor;
 }
